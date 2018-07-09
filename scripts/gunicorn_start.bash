4b7cfc51f386627ba743611dbedf8a933c2512ab
voith <voithjm1@gmail.com>
2018-07-10 01:04:04 +0530

    change python alias

--- scripts/gunicorn_start.bash
+++ scripts/gunicorn_start.bash
@@ -12,6 +12,7 @@ DJANGO_WSGI_MODULE=stackXchange.wsgi                 # WSGI module name
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
+alias python=python3.6
 cd $DJANGODIR
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
