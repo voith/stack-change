88b356fdc88e5612fb0ddea436ee2e8a05e7a18a
voith <voithjm1@gmail.com>
2018-07-09 23:09:59 +0530

    allow all hosts

--- stackXchange/settings.py
+++ stackXchange/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'e1)%abshsd9^7lln(lnmjby+6=7=gim$vbmi6&)r4(r@t^$6h@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
