2d11dfc058cdfb9c18b9702c86fb377ab2360e0a
voith <voithjm1@gmail.com>
2018-07-10 01:18:06 +0530

    test with virtualenv

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -26,12 +26,14 @@ touch .env
 sudo add-apt-repository ppa:jonathonf/python-3.6
 sudo apt-get update
 sudo apt-get install -y python3.6
-update-alternatives --install /usr/bin/python python /usr/bin/python3.6 100
+# update-alternatives --install /usr/bin/python python /usr/bin/python3.6 100
 sudo apt-get install -y build-essential libssl-dev libffi-dev python3.6-dev
 sudo apt-get install -y libmysqlclient-dev
 sudo apt install -y python3-pip
-
-sudo python -m pip install -r requirements.txt
+sudo apt install -y virtualenv
+virtualenv -p python3.6 venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 mysql -u $DB_USER --password=$DB_PASSWORD -e "create database $DB_NAME";
 
