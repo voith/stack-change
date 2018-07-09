d3ad8734518a6d619304dc1eeb2f750eae96858c
voith <voithjm1@gmail.com>
2018-07-09 22:46:13 +0530

    added missing mysql dependency and basic sript for setup

---
+++ scripts/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+export LC_ALL=C
+export APP_NAME="stackXchange"
+
+function wait_for_process(){
+    while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 || sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1; do
+        echo "Waiting for a process to finish"
+        sleep 10
+    done
+}
+
+wait_for_process
+wait_for_process && sudo apt-get -y update
+
+cd $HOME/$APP_NAME
+
+git pull origin master
+
+sudo apt-get update
+sudo apt-get install -y mysql-server
+mysql_secure_installation
+
+touch .env
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install -y python3.6
+update-alternatives --install /usr/bin/python python /usr/bin/python3.6 100
+sudo apt-get install -y build-essential libssl-dev libffi-dev python3.6-dev
+sudo apt install -y python3-pip
+
+sudo python -m pip install -r requirements.txt
+
+python manage.py migrate
