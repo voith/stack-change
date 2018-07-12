04c26072bb6a5914bc8ff35c5cf3d3f04c54927f
voith <voithjm1@gmail.com>
2018-07-12 23:27:10 +0530

    fixed user association with new site model

--- app/pipeline.py
+++ app/pipeline.py
@@ -2,7 +2,8 @@ from functools import partial
 
 from toolz import merge
 
-from app.models import User, UserAssociation
+from app.models import User, UserAssociation, StackExchangeSite
+from stackXchange.utils.functional import split_dict_by_keys
 from stackXchange.utils.orm import get_or_create
 from stackXchange.utils.stack_overflow_aouth import StackOverflowOauth
 
@@ -13,7 +14,9 @@ def save_user_profile(username):
     user = User.objects.get(username=username)
     account_id = user.account_id
     user_data = api.get_user_from_account_id(account_id)
-    data_to_inject = {'user_id': user.id}
+    site_data, user_data = split_dict_by_keys(user_data, {'domain', 'name'})
+    site = get_or_create(StackExchangeSite, site_data, {'doamin'})
+    data_to_inject = {'user_id': user.id, 'site_id': site.id}
     modified_user_data = map(partial(merge, data_to_inject), user_data)
     constraint_keys = {'site_name', 'site_user_id'}
     for _user in modified_user_data:
