303d581d2f5de687cb52bc82cfaaeea46d089244
voith <voithjm1@gmail.com>
2018-07-14 01:27:48 +0530

    fix typo

--- scripts/geth_setup.sh
+++ scripts/geth_setup.sh
@@ -4,3 +4,4 @@ wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.12-3768
 tar -xvzf geth-linux-amd64-1.8.12-37685930.tar.gz
 mkdir -p geth-dir
 tar -xvzf geth-linux-amd64-1.8.12-37685930.tar.gz -C geth-dir --strip-components 1
+cp scripts/config/geth.conf /etc/supervisor/conf.d/
