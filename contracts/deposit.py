00faa4962990e39af0082a7781016eaec6ebe4e9
voith <voithjm1@gmail.com>
2018-07-14 15:32:54 +0530

    fixed deploy

--- contracts/deposit.py
+++ contracts/deposit.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-from eth_utils import  to_hex
 from web3 import Web3
 from web3.middleware.geth_poa import geth_poa_middleware
 
@@ -12,12 +11,13 @@ with open(abi_path, 'r') as f:
     abi = json.loads(f.read())
 w3 = Web3(Web3.IPCProvider(IPC_PATH))
 w3.middleware_stack.inject(geth_poa_middleware, layer=0)
-contract = w3.eth.contract(address=to_hex(CONTRACT_ADDRESS), abi=abi)
+contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=abi)
 
 
 def deposit(account_id, amount):
+    w3.personal.unlockAccount(w3.eth.accounts[1], 'voith123')
     tx_hash = contract.functions.deposit(str(account_id)).transact(
-        {'from': w3.eth.accounts[0], 'value': w3.toWei(amount, 'ether')}
+        {'from': w3.eth.accounts[1], 'value': w3.toWei(amount, 'ether')}
     )
     return w3.eth.waitForTransactionReceipt(tx_hash)
 
