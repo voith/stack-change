f1cb6845ae932f101ac7af2d9a960d4c20bed1c6
voith <voithjm1@gmail.com>
2018-07-14 09:44:40 +0530

    fixed deploy script

--- contracts/deploy.py
+++ contracts/deploy.py
@@ -8,17 +8,17 @@ from stackXchange.settings import IPC_PATH
 
 
 def deploy():
-    contract_path = os.path.join(os.getcwd(), 'StackXchange.sol')
+    contract_path = os.path.join(os.getcwd(), 'contracts/StackXchange.sol')
     with open(contract_path) as f:
         contract_code = f.read()
 
     w3 = Web3(Web3.IPCProvider(IPC_PATH))
     w3.middleware_stack.inject(geth_poa_middleware, layer=0)
-
+    w3.personal.unlockAccount(w3.eth.accounts[0], 'voith123')
     compiled_code = compile_source(contract_code)
     contract_interface = compiled_code['<stdin>:StackXchange']
     contract_cls = w3.eth.contract(abi=contract_interface['abi'], bytecode=contract_interface['bin'])
-    tx_hash = contract_cls.constructor().transact()
+    tx_hash = contract_cls.constructor().transact({'from': w3.eth.accounts[0]})
     tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     print('deployed contract to: {0}'.format(tx_receipt.contractAddress))
 
